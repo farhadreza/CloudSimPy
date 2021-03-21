de4c01c86fd2e2015e0b6caf5d93e5de852fcffb
Jack Zhao <emailserver2718@gmail.com>
2021-03-21 17:10:03 -0500

    move core package into playground

---
+++ playground/core/config.py
@@ -0,0 +1,24 @@
+class TaskInstanceConfig(object):
+    def __init__(self, task_config):
+        self.cpu = task_config.cpu
+        self.memory = task_config.memory
+        self.disk = task_config.disk
+        self.duration = task_config.duration
+
+
+class TaskConfig(object):
+    def __init__(self, task_index, instances_number, cpu, memory, disk, duration, parent_indices=None):
+        self.task_index = task_index
+        self.instances_number = instances_number
+        self.cpu = cpu
+        self.memory = memory
+        self.disk = disk
+        self.duration = duration
+        self.parent_indices = parent_indices
+
+
+class JobConfig(object):
+    def __init__(self, idx, submit_time, task_configs):
+        self.submit_time = submit_time
+        self.task_configs = task_configs
+        self.id = idx
