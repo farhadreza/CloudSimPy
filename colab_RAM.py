8a1cd288b6f85a4b134666312d95cf6b51b27487
Jack Zhao <emailserver2718@gmail.com>
2021-04-15 16:13:18 -0500

    no changes

--- colab_RAM.py
+++ colab_RAM.py
@@ -66,13 +66,17 @@ model_dir = './agents/%s' % name
 eval_info_dir = "experiments/data/eval"
 
 # trained_agent_path = "experiments/data/trained_chkpt200/RAM/model.ckpt-200"
-trained_agent_path = None
+
+
 train_info_dir = "/content/drive/MyDrive/GoogleDrive/MyRepo/curr_agents/RAM_MyBrain"
 # ************************ Parameters Setting End ************************
 
 if not os.path.isdir(model_dir):
     os.makedirs(model_dir)
 restore_point = 0
+save_chkpt_every = 10
+trained_agent_path = None
+
 # agent = Agent(name, brain, 1, reward_to_go=True, nn_baseline=True, normalize_advantages=True,
 #               model_save_path='%s/model.ckpt' % model_dir)
 agent = Agent(name, brain, 1, reward_to_go=True, nn_baseline=True, normalize_advantages=True,
@@ -165,7 +169,6 @@ def save_train_info(agent: Agent, itr: int, reward_type=curr_reward_signal_name)
 #                        avg_completion=average_completion(episode), avg_slowdown=average_slowdown(episode))
 
 
-save_chkpt_every = 10
 
 
 def add_hist(name="", value=None):
