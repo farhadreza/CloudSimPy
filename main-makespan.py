6f0b85d85d5a29d30310de4a412b9cdb73b6e9b9
Md Farhadur Reza <farhad9952@gmail.com>
2022-06-02 22:18:27 -0500

    Update main-makespan.py

--- main-makespan.py
+++ main-makespan.py
@@ -494,8 +494,8 @@ if __name__ == '__main__':
      #algo_deep_js()
      #eval_algo_deep_js()
     # set_path()  # for running on command line
-    #train_DeepJS_data200()
-    eval_algo_deep_js()
+    train_DeepJS_data200()
+    #eval_algo_deep_js()
 
 # DeepJS
 # before makespans ([654])
