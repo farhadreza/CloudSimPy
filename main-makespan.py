917e151f1a5b925eab82879656f96c685485ce58
Md Farhadur Reza <farhad9952@gmail.com>
2022-06-06 17:33:15 -0500

    Update main-makespan.py

--- main-makespan.py
+++ main-makespan.py
@@ -404,8 +404,8 @@ def eval_algo_deep_js():
     # chkpt_path = "/Users/jackz/Documents/P_Macbook/Laptop/Git_Workspace/DataScience/MachineLearning/MyForks/CloudSimPy/playground/Non_DAG/launch_scripts/agents/training/chkpt_120.pkl-5"
     #chkpt_path = "/CloudSimPy/playground/Non_DAG/launch_scripts/agents/training/avgCompletionReward/chkpt_180_mkspan.pkl-10"
     #chkpt_path= "/content/CloudSimPy/curr_agents/MyRAS/brain_My_RAS_30.pkl"
-    #chkpt_path= "/content/CloudSimPy/curr_agents/RAC/brain_RAC_200.pkl"
-    chkpt_path= None
+    chkpt_path= "curr_agents/RAC/brain_RAC_50.pkl"
+    #chkpt_path= None
     agent = Agent(name, brain, 1, reward_to_go=True, nn_baseline=True, normalize_advantages=True,
                   model_save_path='%s/model.ckpt' % eval_info_dir, restore_path=chkpt_path)
     tic = time.time()
